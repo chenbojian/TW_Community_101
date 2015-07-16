1d8e8de8535f8677ee6b462babeda5febb42a56b
chenbojian <chenbojian@foxmail.com>
2015-07-16 22:23:37 +0800

    Use JdbcTemplate to make sure database works fine

--- core/src/test/java/com/community101/core/DatabaseTest.java
+++ core/src/test/java/com/community101/core/DatabaseTest.java
@@ -1,30 +1,29 @@
 package com.community101.core;
 
-import org.hibernate.SessionFactory;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-import org.springframework.test.context.transaction.TransactionConfiguration;
-import org.springframework.transaction.annotation.Transactional;
 
-import static org.hamcrest.CoreMatchers.is;
-import static org.hamcrest.MatcherAssert.assertThat;
+import javax.sql.DataSource;
 
 /**
  * Created by chenbojian on 7/16/15.
  */
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = "classpath:applicationContext.xml")
-@TransactionConfiguration
-@Transactional
+//@TransactionConfiguration
+//@Transactional
 public class DatabaseTest {
+
     @Autowired
-    private SessionFactory sessionFactory;
+    private DataSource dataSource;
 
     @Test
     public void should_pass_if_database_is_exist() {
-        sessionFactory.getCurrentSession().flush();
+        JdbcTemplate jdbcTemplate = new JdbcTemplate(dataSource);
+        jdbcTemplate.queryForList("SELECT * FROM GOODS");
     }
 }
