71e23214c63bf8d696d44725cde4ca9d5db5f14f
chenbojian <chenbojian@foxmail.com>
2015-07-16 17:59:57 +0800

    add hibernate config. if database is exist, test in core should pass.

--- core/src/test/java/com/community101/core/FakeTest.java
+++ core/src/test/java/com/community101/core/FakeTest.java
@@ -1,6 +1,13 @@
 package com.community101.core;
 
+import org.hibernate.SessionFactory;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.test.context.transaction.TransactionConfiguration;
+import org.springframework.transaction.annotation.Transactional;
 
 import static org.hamcrest.CoreMatchers.is;
 import static org.hamcrest.MatcherAssert.assertThat;
@@ -8,10 +15,18 @@ import static org.hamcrest.MatcherAssert.assertThat;
 /**
  * Created by chenbojian on 7/16/15.
  */
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "classpath:applicationContext.xml")
+@TransactionConfiguration
+@Transactional
 public class FakeTest {
+    @Autowired
+    private SessionFactory sessionFactory;
+
     @Test
-    public void should_not_pass() {
+    public void should_pass_if_database_is_exist() {
+        sessionFactory.getCurrentSession().flush();
         System.out.println("hello, this is a fake test in core!");
-        assertThat(1, is(2));
+        assertThat(1, is(1));
     }
 }
