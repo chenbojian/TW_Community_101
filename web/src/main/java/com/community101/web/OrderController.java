85f97b48c2cf709e613918b25bb7ae5c3a09477b
chenjian <beyond_peak@qq.com>
2015-07-22 14:52:13 +0800

    deleted old order-manager page

--- web/src/main/java/com/community101/web/OrderController.java
+++ web/src/main/java/com/community101/web/OrderController.java
@@ -12,7 +12,6 @@ import com.community101.core.service.OrdersService;
 import com.google.gson.Gson;
 import com.community101.core.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
@@ -34,21 +33,6 @@ public class OrderController {
     private GoodsService goodsService;
     private UserService userService;
 
-    private OrderBuilder orderBuilder = new OrderBuilder();
-    Orders order1 = orderBuilder.givenBuilder()
-            .withAddress("community101 unit 5 602")
-            .withId(1)
-            .withUser(new User())
-            .withStatus("new")
-            .withPrice(1200)
-            .build();
-    Orders order2 = orderBuilder.givenBuilder()
-            .withAddress("community101 unit 5 302")
-            .withId(1)
-            .withUser(new User())
-            .withStatus("dispatching")
-            .withPrice(1000)
-            .build();
     @Autowired
     public OrderController(OrdersService ordersService, GoodsService goodsService, UserService userService){
         this.ordersService = ordersService;
@@ -56,47 +40,6 @@ public class OrderController {
         this.userService = userService;
     }
 
-    @RequestMapping("/all")
-    public List<Orders> listAllOrders() {
-        List<Orders> ordersList = new ArrayList<Orders>();
-        ordersList.add(order1);
-        ordersList.add(order2);
-        return ordersList;
-    }
-
-    @RequestMapping("/orderManager")
-    public ModelAndView orderManagerPage() {
-        ModelAndView modelAndView = new ModelAndView("order-manager");
-        List<Orders> newOrdersList = ordersService.listNewOrders();
-        List<Orders> dispatchingOrdersList = ordersService.listDispatchingOrders();
-        List<Orders> completedOrdersList = ordersService.listCompletedOrders();
-        List<OrderInOrderManagerDTO> newOrders = transferOrder(newOrdersList);
-        List<OrderInOrderManagerDTO> dispatchingOrders = transferOrder(dispatchingOrdersList);
-        List<OrderInOrderManagerDTO> completedOrders = transferOrder(completedOrdersList);
-
-        modelAndView.addObject("dispatchingOrdersList", dispatchingOrders);
-        modelAndView.addObject("completedOrdersList", completedOrders);
-        return modelAndView;
-    }
-
-    @RequestMapping("/newOrder")
-    @ResponseBody
-    public void newOrder(int count, HttpServletResponse response) throws Exception {
-        PrintWriter writer = response.getWriter();
-        List<Orders> ordersList = ordersService.listNewOrders();
-        List<OrderInOrderManagerDTO> orders = new ArrayList<OrderInOrderManagerDTO>();
-        for(Orders order:ordersList){
-            OrderInOrderManagerDTO orderDTO = new OrderInOrderManagerDTO();
-            orderDTO.setId(order.getId());
-            orderDTO.setTotalPrice(order.getTotalPrice());
-            orders.add(orderDTO);
-        }
-        if(count<ordersList.size()){
-            OrderInOrderManagerDTO order = orders.get(count);
-            String json = gson.toJson(order);
-            writer.print(json);
-        }
-    }
     //angular
     @RequestMapping("/newOrders")
     @ResponseBody
@@ -185,62 +128,4 @@ public class OrderController {
         return orders;
     }
 
-    class OrderBuilder{
-
-        private long id;
-        private User user;
-        private String address;
-        private Timestamp createTime;
-        private String status;
-        private int price;
-
-        public OrderBuilder() {
-        }
-
-        public OrderBuilder givenBuilder() {
-            return this;
-        }
-
-        public OrderBuilder withId(long id) {
-            this.id = id;
-            return this;
-        }
-
-        public OrderBuilder withUser(User user) {
-            this.user = user;
-            return this;
-        }
-
-        public OrderBuilder withAddress(String address) {
-            this.address = address;
-            return this;
-        }
-
-        public OrderBuilder withTimestamp(Timestamp createTime) {
-            this.createTime = createTime;
-            return this;
-        }
-
-        public OrderBuilder withStatus(String status) {
-            this.status = status;
-            return this;
-        }
-
-        public OrderBuilder withPrice(int price) {
-            this.price = price;
-            return this;
-        }
-
-        public Orders build() {
-            Orders order = new Orders();
-            order.setId(this.id);
-            order.setUser(this.user);
-            order.setAddress(this.address);
-            order.setCreateTime(this.createTime);
-            order.setStatus(this.status);
-            order.setTotalPrice(1200);
-            return order;
-        }
-
-    }
 }
