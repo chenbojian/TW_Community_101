1123a6b2a02244df591b8acde3d6d244873a6a4c
Wang Tao <miffyliye@gmail.com>
2015-07-27 14:25:27 +0800

    extract common html block as template

--- web/src/main/webapp/js/angular/order-manager-controller.js
+++ web/src/main/webapp/js/angular/order-manager-controller.js
@@ -192,4 +192,31 @@ App.controller("orderManagerController", function($scope, $http){
     get_new_orders_list();
     get_dispatching_orders_list();
     get_completed_orders_list();
+
+    $scope.getGoodsDetail = function() {
+
+        var url = "/web/api/customer/order/goods?" + "&goodsId=" + this.goods.id;
+
+        $.ajax({
+            url: url,
+            type: "get",
+            success:function(data) {
+                var html = $("#goodsDetail").html();
+                html = html.replace("[goodsname]", data.name);
+                html = html.replace("[goodsdescription]", data.description);
+                html = html.replace("[goodsprice]", data.price/100);
+                //alert(html);
+
+                $("#goodsDetail").html(html);
+                $("#goodsDetail").modal();
+                $("#goodsDetailName").html(data.name);
+                $("#goodsDetailDescription").html(data.description);
+                var price = data.price/100;
+                $("#goodsDetailPrice").html(price.toFixed(2));
+                $("#goodsDetailPicture").attr("src",data.pic);
+
+            }
+        });
+    }
+
 });
