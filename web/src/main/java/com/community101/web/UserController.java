eda1db254aacbba74baab1879d431a8f81774e5e
chenjian <beyond_peak@qq.com>
2015-07-28 18:59:04 +0800

    fix login problem

--- web/src/main/java/com/community101/web/UserController.java
+++ web/src/main/java/com/community101/web/UserController.java
@@ -4,19 +4,18 @@ import com.community101.core.User;
 import com.community101.core.service.UserService;
 import com.google.gson.Gson;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpRequest;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.ModelAndView;
 
+import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 /**
  * Created by chenjian on 7/27/15.
  */
 @RestController
-@RequestMapping("/api/customer")
+@RequestMapping("/customer")
 public class UserController {
-    Gson gson = new Gson();
     private UserService userService;
 
     @Autowired
@@ -25,69 +24,37 @@ public class UserController {
     }
 
     @RequestMapping(value = "/signIn", method = RequestMethod.POST)
-    public void signIn(@RequestBody String data, HttpSession session){
-        UserWithSMSCode o = gson.fromJson(data, UserWithSMSCode.class);
-        User user = userService.findUserByTel(o.getTelPhone());
+    public ModelAndView signIn(String telPhone, String password, String SMS_code, HttpSession session){
+        User user = userService.findUserByTel(telPhone);
         if(user!=null){
-            user.setPassword(o.getPassword());
+            user.setPassword(password);
             userService.updateUser(user);
             session.setAttribute("userId",user.getId());
         }else{
             user = new User();
-            user.setTelPhone(o.getTelPhone());
-            user.setPassword(o.getPassword());
+            user.setTelPhone(telPhone);
+            user.setPassword(password);
             userService.addUser(user);
-            user = userService.findUserByTel(o.getTelPhone());
+            user = userService.findUserByTel(telPhone);
             session.setAttribute("userId",user.getId());
         }
+        return new ModelAndView("redirect:/index.html");
     }
 
 
     @RequestMapping(value = "/login", method = RequestMethod.POST)
-    public String login(@RequestBody String data, HttpSession session){
-        System.out.println(data);
+    public void login(String telPhone, String password, HttpSession session, HttpServletResponse response) throws Exception{
+        User user = userService.findUserByTel(telPhone);
+        if(user!=null){
+            if(user.getPassword().equals(password)){
+                session.setAttribute("userId", user.getId());
+                System.out.println("success");
+                response.sendRedirect("../index.html");
+            }else{
+                System.out.println("failed");
+                response.getWriter().write("<script language=javascript>alert('password is wrong!');window.location.href='login.html';</script>");
+            }
+        }else response.getWriter().write("<script language=javascript>alert('telPhone is not sign in!');window.location.href='login.html';</script>");
 
-        User u = gson.fromJson(data, User.class);
-        User user = userService.findUserByTel(u.getTelPhone());
-        if(user.getPassword().equals(u.getPassword())){
-            session.setAttribute("userId", user.getId());
-            System.out.println("success");
-            return "success";
-        }else{
-            System.out.println("failed");
-            return "failed";
-        }
-    }
-
-    class UserWithSMSCode{
-        private String telPhone;
-        private String password;
-        private String SMS_code;
-
-        public String getSMS_code() {
-            return SMS_code;
-        }
-
-        public void setSMS_code(String SMS_code) {
-            this.SMS_code = SMS_code;
-        }
-
-        public String getPassword() {
-
-            return password;
-        }
-
-        public void setPassword(String password) {
-            this.password = password;
-        }
-
-        public String getTelPhone() {
-
-            return telPhone;
-        }
-
-        public void setTelPhone(String telPhone) {
-            this.telPhone = telPhone;
-        }
     }
 }
