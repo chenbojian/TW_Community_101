4af5b86a98ce440c133a8b797b7d77165d382add
Wang Tao <miffyliye@gmail.com>
2015-07-28 17:26:57 +0800

    add navigation bar templates

---
+++ web/src/main/webapp/js/adminNaviBar.js
@@ -0,0 +1,5 @@
+/**
+ * Created by MiffyLiye on 28/07/2015.
+ */
+document.body.innerHTML = '<div id="navigationBar"></div>' + document.body.innerHTML;
+$( "#navigationBar" ).load( "/web/html/administratorNavigationBar.html" );
