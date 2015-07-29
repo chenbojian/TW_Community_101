15f45f22892cfbe7b178b36795d9acd9d0997cf4
chenjian <beyond_peak@qq.com>
2015-07-29 17:25:10 +0800

    divide admin part from web

---
+++ admin/src/main/webapp/js/adminNaviBar.js
@@ -0,0 +1,5 @@
+/**
+ * Created by MiffyLiye on 28/07/2015.
+ */
+document.body.innerHTML = '<div id="navigationBar"></div>' + document.body.innerHTML;
+$( "#navigationBar" ).load( "/admin/html/administratorNavigationBar.html" );
