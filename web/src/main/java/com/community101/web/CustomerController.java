9a7c857c4b370c03471a9a1b5e1c64d609f0d4df
Wang Tao <miffyliye@gmail.com>
2015-07-30 13:31:30 +0800

    add a redirecting page as default

--- web/src/main/java/com/community101/web/CustomerController.java
+++ web/src/main/java/com/community101/web/CustomerController.java
@@ -10,34 +10,34 @@ import org.springframework.web.servlet.ModelAndView;
  */
 
 @RestController
-@RequestMapping("")
+@RequestMapping("/customer")
 public class CustomerController {
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public ModelAndView getIndex() {
         return new ModelAndView("index");
     }
 
-    @RequestMapping(value = "/customer/order-history/", method = RequestMethod.GET)
+    @RequestMapping(value = "/order-history/", method = RequestMethod.GET)
     public ModelAndView getOrderHistory() {
         return new ModelAndView("orderHistory");
     }
 
-    @RequestMapping(value = "/customer/shopping-cart/", method = RequestMethod.GET)
+    @RequestMapping(value = "/shopping-cart/", method = RequestMethod.GET)
     public ModelAndView getShoppingCart() {
         return new ModelAndView("shoppingCart");
     }
 
-    @RequestMapping(value = "/customer/order-details/", method = RequestMethod.GET)
+    @RequestMapping(value = "/order-details/", method = RequestMethod.GET)
     public ModelAndView getOrderDetails() {
         return new ModelAndView("orderDetails");
     }
 
-    @RequestMapping(value = "/customer/signup/", method = RequestMethod.GET)
+    @RequestMapping(value = "/signup/", method = RequestMethod.GET)
     public ModelAndView getSignUp() {
         return new ModelAndView("signup");
     }
 
-    @RequestMapping(value = "/customer/login/", method = RequestMethod.GET)
+    @RequestMapping(value = "/login/", method = RequestMethod.GET)
     public ModelAndView getLogIn() {
         return new ModelAndView("login");
     }
---
+++ web/src/main/webapp/default.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: chenbojian
+  Date: 7/30/15
+  Time: 12:25 PM
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+<head>
+  <title></title>
+</head>
+<body onload="parent.location='./customer/'">
+
+</body>
+</html>
