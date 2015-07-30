72f4cf1b43dd4858e6a54805fb74433d1c3b9851
chenbojian <chenbojian@foxmail.com>
2015-07-30 16:31:05 +0800

    remove unused files in admin project.

--- admin/src/main/webapp/js/adminNaviBar.js
+++
@@ -1,5 +0,0 @@
-/**
- * Created by MiffyLiye on 28/07/2015.
- */
-document.body.innerHTML = '<div id="navigationBar"></div>' + document.body.innerHTML;
-$( "#navigationBar" ).load( "/admin/html/administratorNavigationBar.html" );
