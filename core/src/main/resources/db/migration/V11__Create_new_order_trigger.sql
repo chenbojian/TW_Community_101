c6637dbe2458cfc66f082b3a5d74b39d12f3810a
jiaoming37 <13366648680@163.com>
2015-08-03 18:20:39 +0800

    delete community101 word from v10.1 and v11

--- core/src/main/resources/db/migration/V11__Create_new_order_trigger.sql
+++ core/src/main/resources/db/migration/V11__Create_new_order_trigger.sql
@@ -1,3 +1,3 @@
-CREATE TRIGGER community101.addNewOrderTrigger after insert ON community101.ORDERS
+CREATE TRIGGER addNewOrderTrigger after insert ON ORDERS
 FOR EACH ROW
 insert into NEW_ORDERS(order_id) values(NEW.id);
