4470d91bbf7fe95a3af70b6c3d409a09e903b709
Wang Tao <miffyliye@gmail.com>
2015-08-04 11:21:40 +0800

    fix a bug introduced during refactoring (without tests) in order history controller

--- web/src/main/webapp/js/app/controller/order_history_controller.js
+++ web/src/main/webapp/js/app/controller/order_history_controller.js
@@ -1,12 +1,11 @@
 angular.module('webApp')
     .controller('OrderHistoryController', function($scope, $http, $cookies, $resource, SharedFunctions) {
 
+    $scope.message = '';
+
     var orderHistoryResource = $resource(env.contextPath + "/api/order/history");
     $scope.order_list = orderHistoryResource.query();
-
-    $scope.message = '';
-    $scope.order_list = null;
-
+        
     $scope.getOrderGoodsDetail = SharedFunctions.getOrderGoodsDetail;
 
     $scope.launchOrderDetails = function() {
